819a0d9de0ae0be90f218f5705218921e6ad0c30
Alfredo Fomitchenko <a.fomitchenko@reply.it>
2022-02-21 14:13:15 +0100

    Asset is not subscriptable

--- great_expectations/datasource/data_connector/configured_asset_file_path_data_connector.py
+++ great_expectations/datasource/data_connector/configured_asset_file_path_data_connector.py
@@ -242,7 +242,7 @@ class ConfiguredAssetFilePathDataConnector(FilePathDataConnector):
         ):
             # batch_spec_passthrough from data_asset
             batch_spec_passthrough = deepcopy(
-                self.assets[data_asset_name]["batch_spec_passthrough"]
+                self.assets[data_asset_name].batch_spec_passthrough
             )
             batch_definition_batch_spec_passthrough = (
                 deepcopy(batch_definition.batch_spec_passthrough) or {}
